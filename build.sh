9e4e3b8824f771f981375e40c60bce7a9724a685
Stephen Pascoe <Stephen.Pascoe@stfc.ac.uk>
2014-03-14 09:55:31 +0100

    Notebook updates

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ HERE=$PWD
 BUILD_DIR=$HERE/build
 LATEX_TEMPLATE=article
 
+#!TODO: replace http://raw.github.com/SciTools/courses/master/course_content/
+
 pushd $BUILD_DIR
 for nb in $HERE/*.ipynb
 do
